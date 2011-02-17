c964cdf38f930934a170c41622ccac2ed845c73b
equaeghe <equaeghe@sysopweg>
2011-02-17 18:49:13 +0100

    initial python code

---
+++ murasyp/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1'
+__release__ = __version__
+
+import cdd
+import collections
+
+class RealValFunc(collections.MutableMapping, cdd.NumberTypeable):
+      """A real-valued function"""
+      pass
