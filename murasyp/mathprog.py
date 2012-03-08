648e7597f7c5eac0fda4b31997722aa2374e795a
Erik Quaeghebeur <Erik.Quaeghebeur@UGent.be>
2012-03-08 18:42:11 +0100

    finish undocumented/debugready version of CONEstrip

--- murasyp/mathprog.py
+++ murasyp/mathprog.py
@@ -1,5 +1,7 @@
+# coding: utf-8
+
 from murasyp.vectors import Vector, Polytope
-from cdd import Matrix, RepType, Polyhedron
+from cdd import Matrix, RepType, Polyhedron, LPObjType, LinProg, LPStatusType
 
 def vf_enumeration(data=[]):
     """Perform vertex/facet enumeration
@@ -28,46 +30,60 @@ def vf_enumeration(data=[]):
                         for i in ext.lin_set])
     return fv_poly
 
-def feasible(data, mapping):
+def feasible(data, mapping=None):
     """Check feasibility using the CONEstrip algorithm"""
     D = set(Polytope(A) for A in data)
-    h = Vector(mapping)
-    D.add([-h])
+    if (mapping == None) or all(mapping[x] != 0 for x in h):
+        h = None
+    else:
+        h = Vector(mapping)
+        D.add([-h])
     coordinates = list(frozenset.union(*(A.domain() for A in D)))
     E = [[vector for vector in A] for A in D]
+    #print(E)
     while (E != []):
         k = len(E)
         L = [len(A) for A in E]
         l = sum(L)
-        mat = Matrix([[0] + [v[x] for v in A for A in E] + k * [0]
-                      for x in coordinates] + # cone-constraints
-                     [[0] + [int(w == v) for w in A for A in E] + k * [0]
-                      for v in A for A in E] + # μ >= 0
-                     [[-1] + [int(w == -h) for w in A for A in E] + k * [0]] +
-                     [[0] + l * [0] + [int(B == A) for B in E]
-                      for A in E] + # τ >= 0
-                     [[1] + l * [0] + [-int(B == A) for B in E]
-                      for A in E] + # τ <= -1
-                     [[0] + l * [0] + k * [1]] + # (sum of τ_A) >= 0
-                     [[0] + [int(w == v) for w in A for A in E]
-                          + [-int(B == A) for B in E]
-                      for v in A for A in E], # τ_A <= μ_A
-                     number_type='fraction')
+        mat = Matrix([[0] + l * [0] + k * [0]], number_type='fraction')
+        mat.extend([[0] + [v[x] for A in E for v in A  ] + k * [0]
+                    for x in coordinates], linear=True) # cone-constraints
+        mat.extend([[0] + [int(w == v) for B in E for w in B] + k * [0]
+                    for A in E for v in A]) # mu >= 0
+        mat.extend([[1] + l * [0] + [-int(B == A) for B in E]
+                    for A in E]) # tau <= 1
+        mat.extend([[0] + l * [0] + [int(B == A) for B in E]
+                    for A in E]) # tau >= 0
+        mat.extend([[-1] + l * [0] + k * [1]]) # (sum of tau_A) >= 1
+        mat.extend([[0] + [int(w == v) for B in E for w in B]
+                        + [-int(B == A) for B in E]
+                    for A in E for v in A]) # tau_A <= mu_A for all A
+        if h != None: # mu_{-h} >= 1
+            mat.extend([[-1] + [int(w == -h) for A in E for w in A] + k * [0]])
         mat.obj_type = LPObjType.MAX
-        mat.obj_func = tuple([0] + l * [0] + k * [1])
+        mat.obj_func = tuple([0] + l * [0] + k * [1]) # (constant, mu, tau)
+        #print(mat)
         lp = LinProg(mat)
         lp.solve()
         if lp.status == LPStatusType.OPTIMAL:
-            sol = LinProg.primal_solution
-            F = [E[n] for n in range(0, k) if sol[1 + l:][n] == 1]
-            if all(all(sol[sum(L[0:n]):sum(L[0:n]) + L[n]][m] == 0
-                       for m in range(0, L[n]))
-                   for n in range(0, k) if sol[1 + l:][n] == 0):
-                return F
+            sol = lp.primal_solution # (constant, mu, tau)
+            tau = sol[l:]
+            #print(tau)
+            mu = [sol[sum(L[0:n]):sum(L[0:n]) + L[n]] for n in range(0, k)]
+            #print(mu)
+            E = [E[n] for n in range(0, k) if tau[n] == 1]
+            #print(E)
+            if all(all(mu[n][m] == 0 for m in range(0, L[n]))
+                   for n in range(0, k) if tau[n] == 0):
+                E = {Polytope(A) for A in E}
+                #print(E)
+                if h != None:
+                    E = E - {Polytope([-h])}
+                    #print(E)
+                return E
             else:
-                E = F
                 continue
         else:
-            return []
+            return set()
     else:
-        return []
+        return set()
