58f22ef33d2599f7beb680cc8ee6f62739ad3509
Erik Quaeghebeur <Erik.Quaeghebeur@UGent.be>
2013-04-03 15:07:27 +0200

    only make string of floats in _make_rational

--- murasyp/__init__.py
+++ murasyp/__init__.py
@@ -5,7 +5,9 @@ from fractions import Fraction
 
 def _make_rational(value):
     """Make a Fraction of acceptable input"""
+    if type(value) == float:
+        value = str(value) # treat floats as decimal numbers
     try:
-        return Fraction(str(value)) # avoid float to Fraction by going to string
+        return Fraction(value)
     except ValueError:
         print(repr(value) + " is not a Rational number")
